514810aa4ef0b708315540444cae7992394060ae
bradhilton <brad.hilton.nw@gmail.com>
2015-05-11 22:01:30 -0600

    Initial Commit

---
+++ Swiftstraints/Swiftstraints.h
@@ -0,0 +1,19 @@
+//
+//  Swiftstraints.h
+//  Swiftstraints
+//
+//  Created by Bradley Hilton on 5/11/15.
+//  Copyright (c) 2015 Skyvive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftstraints.
+FOUNDATION_EXPORT double SwiftstraintsVersionNumber;
+
+//! Project version string for Swiftstraints.
+FOUNDATION_EXPORT const unsigned char SwiftstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftstraints/PublicHeader.h>
+
+
